f86a8f601c89454bfc5bb636f4ad7bd0935a1582
Enrique Santana <esantanaya@gmail.com>
2018-09-13 19:05:36 -0500

    pep8

--- pdfs.py
+++ pdfs.py
@@ -1,33 +1,39 @@
-import qrcode
-from pagos import Comprobante, Emisor, Receptor, Concepto, DoctoRelacionado
-from pagos import Pago, TimbreFiscalDigital
-from xml.etree import ElementTree as ET
 import os
 import re
+from xml.etree import ElementTree as ET
+
+import qrcode
+from pagos import (Comprobante, Concepto, DoctoRelacionado, Emisor, Pago,
+                   Receptor, TimbreFiscalDigital)
 
 directorio = 'recursos'
 patron = r'\d{2}\-\w{7}\-\w{7}\.xml'
 archivos = os.listdir(directorio)
 archivos_validos = re.findall(patron, '|'.join(archivos))
 
+
 def leer_archivos(archivo):
     tree = ET.parse(archivo)
     root = tree.getroot()
 
+    ns_cfdi = '{http://www.sat.gob.mx/cfd/3}'
+    ns_pago10 = '{http://www.sat.gob.mx/Pagos}'
+    ns_tfd = '{http://www.sat.gob.mx/TimbreFiscalDigital}'
+
     for child in root:
-        if child.tag == '{http://www.sat.gob.mx/cfd/3}Emisor':
+        if child.tag == f'{ns_cfdi}Emisor':
             emisor = Emisor(
                 child.attrib['Nombre'],
                 child.attrib['RegimenFiscal'],
                 child.attrib['Rfc']
             )
-        elif child.tag == '{http://www.sat.gob.mx/cfd/3}Receptor':
+        elif child.tag == f'{ns_cfdi}Receptor':
             receptor = Receptor(
                 child.attrib['Nombre'],
                 child.attrib['Rfc'],
                 child.attrib['UsoCFDI'],
             )
-        elif child.tag == '{http://www.sat.gob.mx/cfd/3}Conceptos':
+        elif child.tag == f'{ns_cfdi}Conceptos':
             conceptos = []
             for grandchild in child:
                 concepto = Concepto(
@@ -39,11 +45,11 @@ def leer_archivos(archivo):
                     grandchild.attrib['ValorUnitario'],
                 )
                 conceptos.append(concepto)
-        elif child.tag == '{http://www.sat.gob.mx/cfd/3}Complemento':
+        elif child.tag == f'{ns_cfdi}Complemento':
             for grandchild in child:
-                if grandchild.tag == '{http://www.sat.gob.mx/Pagos}Pagos':
-                    element_pagos = grandchild.getchildren()
-                if grandchild.tag == '{http://www.sat.gob.mx/TimbreFiscalDigital}TimbreFiscalDigital':
+                if grandchild.tag == f'{ns_pago10}Pagos':
+                    element_pagos = list(grandchild)
+                if grandchild.tag == f'{ns_tfd}TimbreFiscalDigital':
                     timbre = TimbreFiscalDigital(
                         grandchild.attrib['FechaTimbrado'],
                         grandchild.attrib['NoCertificadoSAT'],
@@ -56,7 +62,8 @@ def leer_archivos(archivo):
 
     pagos = []
     for element_pago in element_pagos:
-        element_docto = element_pago.find('{http://www.sat.gob.mx/Pagos}DoctoRelacionado')
+        element_docto = element_pago.find(
+            f'{ns_pago10}DoctoRelacionado')
         docto = DoctoRelacionado(
             element_docto.attrib['Folio'],
             element_docto.attrib['IdDocumento'],
@@ -77,7 +84,7 @@ def leer_archivos(archivo):
         )
         pagos.append(pago)
 
-    if root.tag == '{http://www.sat.gob.mx/cfd/3}Comprobante':
+    if root.tag == f'{ns_cfdi}Comprobante':
         comprobante = Comprobante(
             root.attrib['NoCertificado'],
             root.attrib['Fecha'],
@@ -101,7 +108,8 @@ def leer_archivos(archivo):
     archivo_f33 = os.sep.join(ruta_f33)
 
     with open(archivo_f33, 'r') as f33:
-        lineas = {x.rstrip().split('|')[0]:x.rstrip().split('|')[1:] for x in f33}
+        lineas = {x.rstrip().split('|')[0]: x.rstrip().split('|')[
+            1:] for x in f33}
 
     comprobante.total_letra = lineas['DOCUMENTO'][15]
     comprobante.receptor.clave = lineas['CLIENTE'][0]
@@ -112,10 +120,14 @@ def leer_archivos(archivo):
     comprobante.receptor.pais = lineas['CLIENTE'][12]
     comprobante.receptor.codigo_postal = lineas['CLIENTE'][7]
 
-    img = qrcode.make(f'?re={comprobante.emisor.rfc}&rr={comprobante.receptor.rfc}&tt={comprobante.total}&id={comprobante.timbre.uuid}')
+    img = qrcode.make(
+        f'?re={comprobante.emisor.rfc}&rr={comprobante.receptor.rfc}' +
+        '&tt={comprobante.total}&id={comprobante.timbre.uuid}'
+    )
     img.save(f'{comprobante.emisor.rfc}.png', format='png')
 
     return comprobante
+
 
 if __name__ == '__main__':
     while True:
